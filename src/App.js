e1f62574b9cefa902359cc7f893237f8788c3fbf
Abhishek Jan <abhishek.jan@sendinblue.com>
2022-06-28 22:04:43 +0530

    Button Tests

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [color, setColor] = useState("red");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          Hello World
-        </div>
-        <div>
-          Hello World
-        </div>
-      </header>
-    </div>
+    <>
+      <button
+        style={{
+          backgroundColor: color,
+          padding: "20px",
+          fontSize: "20px",
+          color: "white",
+          borderRadius: "15px",
+          width: "50%",
+          margin: "auto",
+        }}
+        onClick={() => {
+          setColor((prev) => {
+            if (prev == "red") {
+              return "blue";
+            } else return "red";
+          });
+        }}
+      >
+        Change color to {color === "red" ? "blue" : "red"}
+      </button>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen ,fireEvent} from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElements = screen.getAllByText(/hello world/i);
-  expect(linkElements).toHaveLength(3);
+  const buttonElement = screen.getByRole("button", {
+    name: /change color to blue/i,
+  });
+  expect(buttonElement).toHaveStyle({ backgroundColor: "red" });
 });
 
+test("button turns blue when clicked once and red when clicked twice", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", {
+    name: /change color to blue/i,
+  });
+  fireEvent.click(colorButton);
+  
+  expect(colorButton).toHaveStyle({backgroundColor:"blue"});
+
+  expect(colorButton.textContent).toBe("Change color to red");
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({backgroundColor:"red"});
+  expect(colorButton.textContent).toMatch(/change color to blue/i);
+});
